34dbc73cc6f54665072809f6438b5abd78d10cdf
Roberto Molinari <roberto.molinari@hotmail.it>
2020-06-27 12:19:49 -0500

    Update estimation.R
    Correction of starting value algorithm

--- R/estimation.R
+++ R/estimation.R
@@ -46,9 +46,9 @@ start_values <- function(root, shoot, del.t) {
 
   fit.root <- lm(root[2:length(root)] ~ root[1:(length(root)-1)] + shoot[1:(length(shoot)-1)] - 1)
   fit.shoot <- lm(shoot[2:length(shoot)] ~ shoot[1:(length(shoot)-1)] + root[1:(length(root)-1)] - 1)
-  phi.gm.root <- ar1_to_gm(c(abs(fit.root$coefficients[1]), (summary(fit.root)$sigma)**2), median(del.t))
+  phi.gm.root <- abs(ar1_to_gm(c(abs(fit.root$coefficients[1]), (summary(fit.root)$sigma)**2), median(del.t)))
   psi.root <- fit.root$coefficients[2]
-  phi.gm.shoot <- ar1_to_gm(c(abs(fit.shoot$coefficients[1]), (summary(fit.shoot)$sigma)**2), median(del.t))
+  phi.gm.shoot <- abs(ar1_to_gm(c(abs(fit.shoot$coefficients[1]), (summary(fit.shoot)$sigma)**2), median(del.t)))
   psi.shoot <- fit.shoot$coefficients[2]
 
   theta.start <- as.numeric(c(log(phi.gm.root[1]), logit2(ifelse(abs(psi.root) >= 1, sign(psi.root)*0.99, psi.root)*2), log(median(del.t)), log(phi.gm.root[2]),
